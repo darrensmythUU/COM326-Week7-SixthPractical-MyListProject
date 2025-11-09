13e9127a9fe6df334ae4c5713fe6b9e6f072db89
Kiphea <djsmyth05@gmail.com>
2025-11-09 12:20:46 +0000

    Task 3 Complete

--- SixthPractical-MyListProject/Program.cs
+++ SixthPractical-MyListProject/Program.cs
@@ -2,6 +2,26 @@
 {
     public static void Main(string[] args)
     {
+        // Task 2 Beginning
         List<string> months = new List<string>();
+
+        // Task 3 Beginning
+        months.Add("January");
+        months.Add("February");
+        months.Add("March");
+        months.Add("April");
+        months.Add("May");
+        months.Add("June");
+        months.Add("July");
+        months.Add("August");
+        months.Add("September");
+        months.Add("October");
+        months.Add("November");
+        months.Add("December");
+
+        foreach (string month in months)
+        {
+            Console.WriteLine(month);
+        }
     }
 }
